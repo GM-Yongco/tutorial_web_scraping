216840dbac05a0ba3314d32bbe6e050c6922ff8a
Giulio Yongco <yongcogiulio125@gmail.com>
2024-05-24 20:39:32 +0800

    trying to download the links from the chapters

--- manga-dynasty_scans.py
+++ manga-dynasty_scans.py
@@ -41,6 +41,18 @@ def get_html(url:str = "https://chihuahuaspin.com/"):
 
 	return html
 
+def download_img(url:str = "https://dynasty-scans.com/system/releases/000/041/912/05.webp", file_name:str = "a.png") -> None:
+	# check if scraped folder exists
+	path = "00_Scraped/"
+	if(os.path.exists(path) == False):
+		os.makedirs(path)
+
+	path = "00_Scraped/" + file_name
+	try:
+		urlretrieve(url, path)
+	except Exception as e:
+		separator(f"error: {e}")
+
 # ========================================================================
 # MAIN 
 # ========================================================================
@@ -51,10 +63,11 @@ def main():
 	title = "the_guy_she_was_interested_in_wasnt_a_guy_at_all"
 	link = DYNASTY + "/series/" + title
 
-	html = get_html(link)
+	html:str = get_html(link)
 	# write(html)
 
 	# filter for chapter links ===========================================
+	separator("getting chapter links")
 	chapter_links:list = []
 	
 	for element in bs(html, "html.parser").find_all("dd"):
@@ -66,11 +79,23 @@ def main():
 	# write(write_links)
 
 	# filter for image links from every chapter link =====================
+	separator("getting image links")
 	html = get_html(chapter_links[0])
-	write(html)
-		
+	divs:list = (bs(html, "html.parser").find_all("div"))
 
+	# gets the number of pannels in the chapter
+	num_pannels:int = len((divs[-1]).find_all("a"))
+	
+	separator("downloading images in this chapter")
+	# gets the link for the first pannel
+	# note the pannels have similar links in chapter but not out of chapter
+	img_link = DYNASTY + str(divs[-2].find("img").get("src"))
+	for i in range(1, num_pannels-1):
+		img_link = f"{img_link[:-7]}{i:02d}.webp"
+		print(img_link)
+		download_img(img_link, f"{i}.jpeg")
 
+# ========================================================================
 
 if __name__ == '__main__':
 	separator("START")
