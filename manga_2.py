61a5eaf6e67a0ba9236e6d4fdc3f36f650eb3993
Giulio Yongco <yongcogiulio125@gmail.com>
2024-07-02 23:25:47 +0800

    start of testing

--- manga_2.py
+++ manga_2.py
@@ -61,17 +61,24 @@ class DynastyMangaChapter(MangaChapter):
 		separator("GOT PANNEL LINKS")
 
 # ========================================================================
+class DynastyMangaDetails(MangaDetails):
+	def get_chapter_links()->None:
+		pass
+
+# ========================================================================
 # MAIN
 # ========================================================================
 
 if __name__ == '__main__':
 	separator("START")
 
+	manga_link:str = "https://dynasty-scans.com/series/the_guy_she_was_interested_in_wasnt_a_guy_at_all"
+	manga_title:str = "The Guy She Was Interested in Wasn't a Guy At All"
+	manga: DynastyMangaDetails = DynastyMangaDetails(manga_link = manga_link, manga_title = manga_title)
+
 	chapter_link = "https://dynasty-scans.com/chapters/the_guy_she_was_interested_in_wasnt_a_guy_at_all_ch00"
 	chapter: DynastyMangaChapter = DynastyMangaChapter(chapter_link)
 
-	chapter.get_pannel_links()
 	chapter.create_folder()
-	chapter.download_chapter()
 
 	separator("END")
