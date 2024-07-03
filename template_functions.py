0152d8ad4cdda4494b15b504ed094de55af86350
Giulio Yongco <yongcogiulio125@gmail.com>
2024-07-04 04:24:16 +0800

    added the selenium version of the webscraper from the reddit project

--- template_functions.py
+++ template_functions.py
@@ -5,6 +5,7 @@
 
 import os
 import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from urllib.request import Request, urlopen, urlretrieve
 
@@ -15,6 +16,13 @@ from urllib.request import Request, urlopen, urlretrieve
 def separator(x:str = "SECTION") -> None:
 	ret_val = f"\n {x} {'-' * (40 - len(x))}\n"
 	print(ret_val)
+
+def decorator_start_end(func):
+	def wrapper():
+		separator(f"START\t: {func.__name__}")
+		func()
+		separator(f"END\t: {func.__name__}")
+	return wrapper
 
 # ========================================================================
 # FUNCTIONS TEST
@@ -38,9 +46,10 @@ def write(content:str = "test", file_name:str = "a.txt") -> None:
 	the_file.close
 
 # ========================================================================
-# FUNCTIONS SCRAPE
+# FUNCTIONS SCRAPE IMG
 # ========================================================================
 
+@decorator_start_end
 def download_img(
 		url:str = "https://xfs-n18.xfspp.com/comic/5003/962/628073bbfe272d6dc98e8269/15928920_1444_2048_1035819.jpeg", 
 		file_name:str = "a.png"
@@ -56,7 +65,12 @@ def download_img(
 	except Exception as e:
 		print(e)
 
-def get_html(url:str = "https://chihuahuaspin.com/")->str:
+# ========================================================================
+# FUNCTIONS SCRAPE HTML
+# ========================================================================
+
+@decorator_start_end
+def get_html_urllib(url:str = "https://chihuahuaspin.com/")->str:
 	request_site = Request(url, headers={"User-Agent": "Mozilla/5.0"})
 	page = urlopen(request_site)
 
@@ -65,7 +79,21 @@ def get_html(url:str = "https://chihuahuaspin.com/")->str:
 
 	return html
 
-def get_html_old(url:str = "https://chihuahuaspin.com/")->bs:
+@decorator_start_end
+def get_html_request(url:str = "https://chihuahuaspin.com/")->bs:
 	response:requests = requests.get(url)
 	soup:bs = bs(response.text, 'html.parser')
+	return soup
+
+@decorator_start_end
+def get_html_selenium(THE_URL):
+	driver = webdriver.Chrome()
+	driver.get(THE_URL)
+	wait_seconds:int = 30
+	driver.implicitly_wait(wait_seconds)
+	page_source = driver.page_source
+	driver.quit()
+
+	soup:bs = bs(page_source, "html.parser")
+
 	return soup
