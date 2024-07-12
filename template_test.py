0cc14e5b939146ed43f68c6fc8705157a9bc0f30
Giulio Yongco <yongcogiulio125@gmail.com>
2024-07-12 19:37:29 +0800

    notes

--- template_test.py
+++ template_test.py
@@ -13,14 +13,16 @@ if __name__ == '__main__':
 	separator("START")
 
 	# get html of a site
-	link_page = "https://dynasty-scans.com/doujins/bocchi_the_rock"
-	print(link_page)
-	write(content = get_html_urllib(link_page))
-	write(get_html_request(link_page), "b.txt")
+
+	# link_page = "https://dynasty-scans.com/doujins/bocchi_the_rock"
+	# print(link_page)
+	# write(content = get_html_urllib(link_page))
+	# write(get_html_request(link_page), "b.txt")
 
 
 	# get img from link
-	# link_image = "https://dynasty-scans.com/system/releases/000/039/165/03-04.webp"
-	# download_img(link_image)
+
+	link_image = "https://dynasty-scans.com/system/releases/000/039/165/03-04.webp"
+	download_img(link_image, "kill.png")
 
 	separator("END")
