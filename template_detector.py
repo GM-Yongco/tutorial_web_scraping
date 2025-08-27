9c1ece349d5a827d2e4d1761d1c7576838fc00c4
Giulio Yongco <yongcogiulio125@gmail.com>
2025-08-27 18:42:55 +0800

    made the detector more general and moved non-general functions

--- template_detector.py
+++ template_detector.py
@@ -9,53 +9,58 @@
 # HEADERS
 # ========================================================================
 
-import os
+from typing import Callable, List
+from utils import section, read, write
 
-from typing import Callable
-from bs4 import BeautifulSoup as bs
-
-from utils import section, read, write, get_soup
-
-# ========================================================================
-# FETCH RELEVANT DATA
-# ========================================================================
-
-def fetch_data_sitting_next_to_me()->str:
-	ret_val:str = ""
-
-	try:
-		soup:bs = get_soup(link="https://mto.to/chapter/3041986")
-		soup:bs = soup.select("optgroup option")[-1]
-
-		ret_val += f"Title: {soup.get_text().strip()}"
-		ret_val += "\n"
-		ret_val += f"Link: https://mto.to/chapter/{soup.get('value').strip()}"
-	except Exception as e:
-		section(f"ERROR IN {'fetch_data':20}\n{e}")
-	
-	return ret_val
+from fetch_data_manga_xbato import fetch_data_sitting_next_to_me, fetch_data_our_sunny_days
+from fetch_data_manga_mangadex import fetch_data_tgswiiwagaa
 
 # ========================================================================
+# CLASS
+# ========================================================================
 
-def detector(fetch_data:Callable = fetch_data_sitting_next_to_me) -> bool:
-	ret_val:bool = False
-	file_name:str = "temp_" + fetch_data.__name__ + ".txt"
+class DetectorDataUpdates():
+	def __init__(
+			self, TOKEN:str = "", 
+			LOG_CHANNEL_ID:str = ""
+		) -> None:
+		self.fetch_data_function_list:List[Callable] = []
+
+	def is_new_record(
+			self,
+			fetch_data:Callable
+		) -> bool:
+		
+		ret_val:bool = False
+
+		file_name:str = "temp_" + fetch_data.__name__ + ".txt"
+		old_record:str = read(file_name)
+		new_record:str = fetch_data()
+
+		# is true when old doesnt coencide with new
+		if new_record == "":
+			# this is an error check
+			pass
+		elif  old_record == new_record:
+			pass
+		else:
+			ret_val = True
+			write(content=new_record, file_name=file_name)
+		
+		return ret_val
+
+	# ====================================================================
 	
-
-	old_record:str = read(file_name)
-	new_record:str = fetch_data()
-
-	# is true when old doesnt coencide with new
-	if new_record == "":
-		# this is an error check
-		pass
-	elif  old_record == new_record:
-		pass
-	else:
-		ret_val = True
-		write(content=new_record, file_name=file_name)
-	
-	return ret_val
+	def fetch_data(self)->None:
+		for fetch_data_function in self.fetch_data_function_list:
+			try:
+				if self.is_new_record(fetch_data_function):
+					print(f"YEA BABY, NEW CHAPTER in {fetch_data_function.__name__}")
+				else:
+					print(f"no new chapter :((( in {fetch_data_function.__name__}")
+			except Exception as e:
+				section(f"ERROR PROCESSING {f'{fetch_data_function.__name__}':20}\n{e}")
+		
 
 # ========================================================================
 # MAIN 
@@ -64,8 +69,10 @@ def detector(fetch_data:Callable = fetch_data_sitting_next_to_me) -> bool:
 if __name__ == '__main__':
 	section("START")
 
-	if detector():
-		print("YEA BABY, NEW CHAPTER")
-	else:
-		print("no new chapter :(((")
+	detector:DetectorDataUpdates = DetectorDataUpdates()
+	detector.fetch_data_function_list.append(fetch_data_sitting_next_to_me)
+	detector.fetch_data_function_list.append(fetch_data_our_sunny_days)
+	detector.fetch_data_function_list.append(fetch_data_tgswiiwagaa)
+	detector.fetch_data()
+
 	section("END")
